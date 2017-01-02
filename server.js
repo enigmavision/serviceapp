810fdc9130b73c7284b833ad8df4f27b8dc044d3
Will Palafox <wpalafox@gmail.com>
2017-01-01 18:40:42 -0600

    Moved Design files to Public and fixed a typo in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require('method-override');
-var exphbs = require('exprees-handlebars');
+var exphbs = require('express-handlebars');
 
 
 // Sets up the Express App
