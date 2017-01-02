9b095d6cd9e7e5e8cb21568bc9195eee0eba33f1
Will Palafox <wpalafox@gmail.com>
2017-01-02 17:58:56 -0600

    Added ajax call for userData

--- Data/users.js
+++ Data/users.js
@@ -6,6 +6,7 @@ var users = [
 		name: "William Palafox",
 		email: "wpalafox@gmail.com",
 		password: "iLoveChica15",
+		cpassword: "iLoveChica15",
 		address: "125 Main Street",
 		zipcode: "78748",
 		avatar: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160825160953-05-week-in-photos-0826-super-169.jpg"
