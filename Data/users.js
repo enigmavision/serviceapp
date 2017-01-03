54f89d20f6a64f660dafb60d11aa795e8ba4153c
Will Palafox <wpalafox@gmail.com>
2017-01-02 21:24:22 -0600

    dummy service provider data

--- Data/users.js
+++ Data/users.js
@@ -11,6 +11,17 @@ var users = [
 		zipcode: "78748",
 		avatar: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160825160953-05-week-in-photos-0826-super-169.jpg"
 
+	},
+
+	{
+		name: "Henry Steven",
+		email: "fakeemail@hmail.com",
+		password: "gotigers1",
+		cpassword: "gotigers1",
+		address: "125 Halo Street",
+		zipcode: "67689",
+		avatar: "http://www.defenders.org/sites/default/files/styles/large/public/tiger-dirk-freder-isp.jpg"
+
 	}
 
 
