35cffaeeb33cf54014e6da86d49a24b98959f000
Will Palafox <wpalafox@gmail.com>
2017-01-04 13:39:13 -0600

    handlebar Rendered userProfile success

--- Routes/api-routes.js
+++ Routes/api-routes.js
@@ -3,12 +3,29 @@ var providers = require("../data/providers.js");
 var reviews = require("../data/reviews.js");
 var serviceRequests = require("../data/serviceRequests.js");  
 
+//Dummy User Data Object 
+var userData = {
+	
+	name:"John Doe",
+	email: "jdoe@gmail.com",
+	password: "iamanderson1",
+	cpassword: "iamanderson1",
+	address: "126 Main Street",
+	zipcode: "98922",
+	avatar: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160825160953-05-week-in-photos-0826-super-169.jpg",
+
+};
+
 module.exports = function(app){
 
 
-	app.get("/api/user", function(req, res){
+	app.get("/user", function(req, res){
 
-		res.render("userProfile", {})
+		res.render("userProfile", {
+			
+			address: userData.name, email: userData.email, address: userData.address, zipcode: userData.zipcode, avatar: userData.avatar   
+			
+			});
 
 	
 	});
