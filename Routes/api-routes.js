d8f5b37b6bba1762dca2d55370e3228644aac694
Will Palafox <wpalafox@gmail.com>
2017-01-04 10:51:29 -0600

    more handlebars setup

--- Routes/api-routes.js
+++ Routes/api-routes.js
@@ -1,14 +1,36 @@
-var users = require("../Data/users.js");
-
+var users = require("../data/users.js");
+var providers = require("../data/providers.js");
+var reviews = require("../data/reviews.js");
+var serviceRequests = require("../data/serviceRequests.js");  
 
 module.exports = function(app){
 
 
-	app.get("/api/users", function(req, res){
+	app.get("/api/user", function(req, res){
 
-		res.json(users);
+		res.render("userProfile", {})
+
 	
 	});
+
+	app.get("/api/provider", function(req, res){
+
+		res.json(providers);
+	
+	});
+
+	app.get("/api/review", function(req, res){
+
+		res.json(reviews);
+	
+	});
+
+	app.get("/api/servicerequests", function(req, res){
+
+		res.json(serviceRequests);
+	
+	});
+
 
 
 }
